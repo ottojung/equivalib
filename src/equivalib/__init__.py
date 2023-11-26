a8875c66b7068b85badaf75e73d9d46707208db8
Otto Jung <otto.jung@vauplace.com>
2023-11-25 19:51:52 -0800

    Add read_type_information.py with function to read type annotations and its tests

--- src/equivalib/__init__.py
+++ src/equivalib/__init__.py
@@ -2,3 +2,4 @@
 # pylint: disable=unused-import,wildcard-import,unused-wildcard-import
 from .bounded_int import *
 from .partially_order import *
+from .read_type_information import *
---
+++ tests/test_read_type_information.py
@@ -0,0 +1,57 @@
+
+from dataclasses import dataclass
+from typing import Literal
+import pytest
+from equivalib.bounded_int import BoundedInt
+import equivalib
+
+
+@dataclass
+class IntervalUnbounded:
+    start: int
+    end: int
+
+
+def test_read_type_information_for_valid_dataclass():
+    expected = {
+        "start": [int],
+        "end": [int],
+    }
+
+    result = equivalib.read_type_information(IntervalUnbounded)
+    assert result == expected, "Expected type information does not match actual result."
+
+
+@dataclass
+class Interval:
+    start: BoundedInt[Literal[0], Literal[999]]
+    end: BoundedInt[Literal[0], Literal[999]]
+
+
+def test_read_type_information_for_valid_generic_dataclass():
+    expected = {
+        "start": [BoundedInt, Literal[0], Literal[999]],
+        "end": [BoundedInt, Literal[0], Literal[999]],
+    }
+
+    result = equivalib.read_type_information(Interval)
+    assert result == expected, "Expected type information does not match actual result."
+
+
+class CustomInterval:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+def test_read_type_information_for_invalid_class():
+    with pytest.raises(TypeError):
+        equivalib.read_type_information(CustomInterval)
+
+
+@dataclass
+class EmptyType:
+    pass
+
+
+def test_read_type_information_empty():
+    assert {} == equivalib.read_type_information(EmptyType)
