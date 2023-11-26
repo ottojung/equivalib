6881b5544128c45deaf75a75dd84e69c1bdac65c
Otto Jung <otto.jung@vauplace.com>
2023-11-25 20:20:52 -0800

    Implement exhaustive int generation

--- src/equivalib/generate_instances.py
+++ src/equivalib/generate_instances.py
@@ -1,10 +1,11 @@
 ## Copyright (C) 2023  Otto Jung
 ## This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import typing
 from typing import Type, Generator, List, Tuple, Any, Optional
 import itertools
 import equivalib
-from equivalib import GeneratorContext
+from equivalib import GeneratorContext, BoundedInt
 
 
 def generate_field_values(ctx: GeneratorContext,
@@ -16,6 +17,14 @@ def generate_field_values(ctx: GeneratorContext,
         assert len(type_signature) == 1
         yield (None, False)
         yield (None, True)
+    elif t == BoundedInt:
+        assert len(type_signature) == 3
+        low_l, high_l = type_signature[1:]
+        low, high = (typing.get_args(low_l)[0], typing.get_args(high_l)[0])
+        assert isinstance(low, int)
+        assert isinstance(high, int)
+        for i in range(low, high + 1):
+            yield (None, i)
     elif len(type_signature) == 1:
         yield from ((k, v) for (k, v) in ctx.assignments.items() if v == t)
     else:
--- tests/test_generate_context.py
+++ tests/test_generate_context.py
@@ -1,8 +1,9 @@
 
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Tuple, Literal
 import pytest
 import equivalib
+from equivalib import BoundedInt
 
 
 @dataclass(frozen=True)
@@ -41,3 +42,55 @@ class BadTuple:
 def test_invalid():
     with pytest.raises(ValueError):
         equivalib.generate_context([BadTuple])
+
+
+
+@dataclass(frozen=True)
+class Inted:
+    is_yes: bool
+    confidence: BoundedInt[Literal[1], Literal[20]]
+
+
+def test_ints():
+    ctx = equivalib.generate_context([Inted])
+    assert ctx.assignments \
+        == {'a': Inted(False, 1),
+            'b': Inted(False, 2),
+            'c': Inted(False, 3),
+            'd': Inted(False, 4),
+            'e': Inted(False, 5),
+            'f': Inted(False, 6),
+            'g': Inted(False, 7),
+            'h': Inted(False, 8),
+            'i': Inted(False, 9),
+            'j': Inted(False, 10),
+            'k': Inted(False, 11),
+            'l': Inted(False, 12),
+            'm': Inted(False, 13),
+            'n': Inted(False, 14),
+            'o': Inted(False, 15),
+            'p': Inted(False, 16),
+            'q': Inted(False, 17),
+            'r': Inted(False, 18),
+            's': Inted(False, 19),
+            't': Inted(False, 20),
+            'u': Inted(True, 1),
+            'v': Inted(True, 2),
+            'w': Inted(True, 3),
+            'x': Inted(True, 4),
+            'y': Inted(True, 5),
+            'z': Inted(True, 6),
+            'aa': Inted(True, 7),
+            'ab': Inted(True, 8),
+            'ac': Inted(True, 9),
+            'ad': Inted(True, 10),
+            'ae': Inted(True, 11),
+            'af': Inted(True, 12),
+            'ag': Inted(True, 13),
+            'ah': Inted(True, 14),
+            'ai': Inted(True, 15),
+            'aj': Inted(True, 16),
+            'ak': Inted(True, 17),
+            'al': Inted(True, 18),
+            'am': Inted(True, 19),
+            'an': Inted(True, 20)}
