e755f35bf5c7e597d11c1cc0767b00087db5ffb9
Otto Jung <otto.jung@vauplace.com>
2024-08-21 14:17:05 -0700

    add a todo for flatten

--- src/equivalib/flatten_type_hierarchy.py
+++ src/equivalib/flatten_type_hierarchy.py
@@ -5,5 +5,7 @@ from equivalib.typeform import TypeForm
 
 
 def flatten_type_hierarchy(types: Iterable[Iterable[TypeForm]]) -> Iterator[TypeForm]:
+    # TODO: sort such that Super types are generated later. This is an optimization.
+
     for group in types:
         yield from group
